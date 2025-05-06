b51a2feb47ecc545e5ce3e9416dda6d84bca0031
Joseph Nguyen <joseph.nguyen@soaren.io>
2025-05-05 21:35:31 -0700

    Using routers, added validation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,12 +2,18 @@ import express, { Request, Response } from "express";
 
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req: Request, res: Response) => {
-  res.send("Hi there!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!");
